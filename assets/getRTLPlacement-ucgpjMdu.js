28e6893e6b6db264361b9b6bcdc70bacba1b5ec1
AbdyInk <abdyxx@gmail.com>
2024-05-10 10:10:02 -0600

    Updates

--- assets/getRTLPlacement-ucgpjMdu.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./DefaultLayout-ilwomWax.js";var a=function(r,t){switch(r){case"right":return e(t)?"left":"right";case"left":return e(t)?"right":"left";default:return r}};export{a as g};
